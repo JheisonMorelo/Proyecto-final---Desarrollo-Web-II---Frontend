6d3c050788bb8e6cc88499d49d9a17a34335befc
Jheison2023 <jmorelosmorales@gmail.com>
2025-06-05 04:07:47 -0500

    Detallamiento de la landing page. Mejoramiento de cómo se ve el nav. ubicación correcta de imágenes en la carpeta 'public/', contextualización de la informaión del footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderLandingpageComponent } from './header-landingpage/header-landingpage.component';
-import { HeroLandingpageComponent } from './hero-landingpage/hero-landingpage.component';
-import { ServiciosDestacadosLandingpageComponent } from './servicios-destacados-landingpage/servicios-destacados-landingpage.component';
-import { SobreNosotrosLandingpageComponent } from './sobre-nosotros-landingpage/sobre-nosotros-landingpage.component';
-import { ClientesSatisfechosLandingpageComponent } from './clientes-satisfechos-landingpage/clientes-satisfechos-landingpage.component';
-import { FooterLandingpageComponent } from './footer-landingpage/footer-landingpage.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @Component({
   selector: 'app-root', 
-  imports: [RouterOutlet,
-    HeaderLandingpageComponent,
-    HeroLandingpageComponent,
-    ServiciosDestacadosLandingpageComponent,
-    SobreNosotrosLandingpageComponent,
-    ClientesSatisfechosLandingpageComponent, 
-    FooterLandingpageComponent],
+  imports: [RouterOutlet, LandingpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { FooterLandingpageComponent } from '../footer-landingpage/footer-landingpage.component';
+import { HeaderLandingpageComponent } from '../header-landingpage/header-landingpage.component';
+import { HeroLandingpageComponent } from '../hero-landingpage/hero-landingpage.component';
+import { ServiciosDestacadosLandingpageComponent } from '../servicios-destacados-landingpage/servicios-destacados-landingpage.component';
+import { SobreNosotrosLandingpageComponent } from '../sobre-nosotros-landingpage/sobre-nosotros-landingpage.component';
+import { ClientesSatisfechosLandingpageComponent } from '../clientes-satisfechos-landingpage/clientes-satisfechos-landingpage.component';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [],
+  imports: [HeaderLandingpageComponent,
+      HeroLandingpageComponent,
+      ServiciosDestacadosLandingpageComponent,
+      SobreNosotrosLandingpageComponent,
+      ClientesSatisfechosLandingpageComponent, 
+      FooterLandingpageComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
